f3c876bfb33d4eec52a3a4fc38ba76656746273c
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-10-22 21:10:48 +0200

    added thoregon and evolux modules as installable components,

---
+++ components/matter.mjs
@@ -0,0 +1,14 @@
+import { ComponentDescriptor } from '/evolux.dyncomponents';
+
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+export default ComponentDescriptor({
+    id :            'matter',
+    displayName:    'Matter',
+    category :      'universe',
+    href :          '/evolux.matter',
+});
