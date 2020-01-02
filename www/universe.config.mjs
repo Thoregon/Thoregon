82c64a5dd7bc209a6ab769f70bdad3468210eef4
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-01-02 22:01:13 +0100

    GUN as reliant module (node)

--- www/universe.config.mjs
+++ www/universe.config.mjs
@@ -11,22 +11,11 @@ import dsysp        from "./dsys.reliant.mjs";
 
 import { myevolux, mythoregon } from '/evolux.universe';
 
+protouniverse.gunpeers = ['https://service.broadcast.green:8765/gun'];
+
 protouniverse.atDawn(async universe => {
     const componentController = Controller.baseCwd('ThoregonComponentController');
     myevolux().components = componentController;
-
-    /*
-     * define all component slots
-     */
-    const t = mythoregon();
-
-    // matter
-    t.matter = {};
-    t.matter.storeadapter = {};
-
-    // tru4D
-    t.dddd = {};
-    t.dddd.storeadapter = {};
 
     // now setup the basic distributed system
     await dsysp(universe);      // first the reliant setup
