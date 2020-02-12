5b7c11b125feffb5b1b410f414a8229cfd7bf508
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-02-12 21:31:45 +0100

    iframeloader structured

--- www/index.mjs
+++ www/index.mjs
@@ -5,26 +5,16 @@
  */
 
 
-import letThereBeLight      from '/evolux.universe';
+import letThereBeLight, { loadIframe }      from '/evolux.universe';
 // import { busy, ready }   from '/evolux.universe';
 
 (async () => {
     try {
         // busy();
         const universe              = await letThereBeLight();
-        loadapp();
+        loadIframe('app.html');
         // ready();
     } catch (err) {
         console.log(err);
     }
 })();
-
-const loadapp = () => {
-    let body = document.getElementsByTagName('body')[0];
-    let app = document.createElement('iframe');
-    app.src = 'app.html';
-    body.appendChild(app);
-    // app.innerHTML = '<a href="app.html"><iframe src="app.html"></iframe></a>';
-    app.contentWindow.universe = window.universe;
-    app.contentWindow.thoregon = window.thoregon;
-}
