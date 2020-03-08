f8b88b50b224eda2216388308be4d398799d4ce8
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-03-08 13:43:26 +0100

    fixed heliots component install

--- test.config.mjs
+++ test.config.mjs
@@ -10,41 +10,5 @@ export default async () => {
     const ComponentDescriptor   = components.ComponentDescriptor;
 
     universe.logger.info("test.config");
-
-    const component = ComponentDescriptor({
-        id :            'Karte',
-        displayName:    'Karte - Browser for the universe',
-        category :      'test',
-        href :          '/thoregon.karte',
-    });
-
-    await components.install(component);
-
-    // install 'gun' to have synced distributed DB available
-    await components.install(component);
-    await components.resolve(component.id);
-    await components.start(component.id);
-
-    /*
-        const channel = ComponentDescriptor({
-            id :            'channel',
-            displayName:    'channel from broadcast green',
-            category :      'test',
-            href :          './lib/channel',
-        });
-
-        await components.install(channel);
-
-        // install 'gun' to have synced distributed DB available
-        await components.install(channel);
-        await components.resolve(channel.id);
-    */
-
-/*
-    let channels = await universe.matter.broadcast.green.channels.val;
-    let mirror = channels.mirror;
-
-    universe.logger.info(mirror);
-*/
 };
 
--- www/test.config.mjs
+++ www/test.config.mjs
@@ -10,19 +10,5 @@ export default async () => {
     const ComponentDescriptor   = components.ComponentDescriptor;
 
     universe.logger.info("test.config");
-
-    const component = ComponentDescriptor({
-        id :            'Karte',
-        displayName:    'Karte - Browser for the universe',
-        category :      'test',
-        href :          '/thoregon.karte',
-    });
-
-    await components.install(component);
-
-    // install 'gun' to have synced distributed DB available
-    await components.install(component);
-    await components.resolve(component.id);
-    await components.start(component.id);
 };
 
