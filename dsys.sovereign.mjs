3b2e460e87001b84b92f9467630cec10908c6543
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-03-08 13:31:51 +0100

    fixed heliots component install

--- dsys.sovereign.mjs
+++ dsys.sovereign.mjs
@@ -10,22 +10,6 @@ const dsysp = async (universe) => {
     const services              = universe.services;
     const components            = services.components;
     const ComponentDescriptor   = components.ComponentDescriptor;
-
-    /*
-     * Components
-     */
-    const www =       ComponentDescriptor({
-        id:             'www',
-        displayName:    'simple webservice connection to components',
-        category:       'universe',
-        href:           '/evolux.web',
-    });
-
-    // install 'www' to have synced distributed DB available
-    await components.install(www);
-    await components.resolve(www.id);
-    await components.start(www.id);
-
 };
 
 export default dsysp;
