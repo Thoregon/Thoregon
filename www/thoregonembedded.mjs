b10db3c82145446d40df3c3cf786af5d39880d30
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-06-18 09:59:46 +0200

    embedded also import components

--- www/thoregonembedded.mjs
+++ www/thoregonembedded.mjs
@@ -4,15 +4,16 @@
  * @author: blukassen
  */
 
-
-import letThereBeLight      from '/evolux.universe';
-// import components           from './@components';
+import letThereBeLight          from '/evolux.universe';
+import { timeout, doAsync }     from '/evolux.universe';
+import components               from './@components';
 
 (async () => {
     try {
         const universe = await letThereBeLight();
-        // await universe.addComponents(components);     // todo: automatize
+        await universe.addComponents(components);     // todo: automatize
 
+        await timeout(300);
         document.dispatchEvent(new CustomEvent('universe', { detail: universe }));
     } catch (err) {
         console.log(err);
