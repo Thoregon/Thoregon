67ff99948b6b0dc4271e3ccc8dd60ac0c2c0a2c5
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-07-29 11:57:47 +0200

    everblack secure private store

--- components/app.test/index.mjs
+++ components/app.test/index.mjs
@@ -4,9 +4,15 @@
  * @author: Bernhard Lukassen
  */
 
+import { doAsync, timeout }                  from '/evolux.universe';
+
 export default {
     app: 'Test'
 };
+
+const rootlocation = 'HmzixZUROc0EqSdsFKyU7eq11';
+const mychannel    = 'channels.mytestchatgroup';
+const location     = `${rootlocation}.${mychannel}`;
 
 const alice = async () => {
     universe.logger.info("Test Everblack Alice");
@@ -16,35 +22,38 @@ const alice = async () => {
     let Channel = universe.everblack.Channel;
 
     let channel = await Channel
-        .at('thatsme.channels.mytestchatgroup')
-        .signon()
-        .create();  // current user is admin
+        .at(location)
+        .createIfMissing();  // current user is admin
 
     await channel.invite('bobB');
 
-    // channel.onMessage(console.log);               // show arriving messages including all previous
+    // channel.onMessage((msg) => console.log('alice>', msg));               // show arriving messages including all previous
     // await channel.send("message 1");
 
+    await universe.identity.leave();
     universe.logger.info("Test Everblack Alice END");
 }
 
 const bob = async () => {
     universe.logger.info("Test Everblack Bob");
 
-    universe.Identity.auth('bobB', 'bobB1');
+    await universe.Identity.auth('bobB', 'bobB1');
 
     let Channel = universe.everblack.Channel;
-    let channel = await Channel.at('thatsme.channels.mytestchatgroup');
-    await channel.join();    // user must be invited or admin
-//   channel.onMessage(console.log);               // show arriving messages including all previous
+    let channel = await Channel
+        .at(location)
+        .join();
+    // channel.onMessage((msg) => console.log('bob>', msg));               // show arriving messages including all previous
 
 //    await channel.send("message 2");
 
+    // await universe.identity.leave();
     universe.logger.info("Test Everblack Bob END");
 }
 
 (async () => {
     await alice();
-    // await bob();
+    await timeout(500);
+    await bob();
 })();
 
