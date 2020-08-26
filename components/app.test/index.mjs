91fbd57802038ab6880b05bccb243c608c3adbe0
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-08-26 10:20:07 +0200

    everblack request queue

--- components/app.test/index.mjs
+++ components/app.test/index.mjs
@@ -12,7 +12,89 @@ export default {
 
 const rootlocation = 'KvjpVDvE4jVuKSlV2JxCxyUP7';
 const mychannel    = 'mytestchatgroup';
-const location     = `${rootlocation}.${mychannel}`;
+const clocation    = `${rootlocation}.${mychannel}`;
+
+const qrootlocation = 'KvjpVDvE4jVuKSlV2JxCxyUQ2';
+const myqueue       = 'queue';
+const qlocation     = `${qrootlocation}.${myqueue}`;
+const pqlocation    = `${qrootlocation}.p${myqueue}`;
+
+const servicepair = {
+    pub  : "tbf4_XSzaAcYqqN_DTwQh-TWKdKHpg4O1V8fQa-P-z8.2tiwh3UaUpxDgXqNBt_7adeZDndfh1aor-sdAxrQkgA",
+    priv : "wCfVo8E3ehSsyqSnpQn_pL-oHmfwPeNxuezvxuzdFQM",
+    epub : "M8b8e_6rq3-zPur0Juc-PiOCWSIItKy37WGqX67CDH4.BW3rYwDgEU8I_R9J2LaE2k2iUVMaQZAXCH_AD6rT-e0",
+    epriv: "ZkL7GfESU2dDnG4Dzk5Vv4d9WoCp3N5anH6vysocWoQ"
+};
+const clientpair  = {
+    pub  : "fak0L-STC5ZqRfbG1DDSIwV1L4vKil2qfzqZO2FvAJ0.vkVdgh56H5mVdvsc5RnB_Ev1QtCLKWUmwOm_aVIqm78",
+    priv : "9UjHqyhMIuvyjKSSa16bqcpyxuUayV8ccLUev2oJ_uA",
+    epub : "7s8xc1QqAAkrYJL6mMhwLnLA8ambhRhcgewJgsDIOJI.5j0DH4elS0IbgmmVkM9IvB_LUpCjn43BIZG21gfUBD4",
+    epriv: "NwEANRzlBSywtX8ySWJw84CtrQWIT0q8AjG7cSlS1Ik"
+};
+
+const service = async () => {
+    universe.logger.info("Test Everblack Service");
+    let Queue = universe.everblack.Queue;
+
+    let queue = await Queue
+        .at(qlocation)
+        .signon(servicepair)    // owner of servicepair is admin
+        .createIfMissing();
+
+    queue.handle((request, response) => {
+        universe.logger.info('Queue Request', request);
+        response.payload = request.payload;
+        response.send();
+    })
+
+    universe.logger.info("Test Everblack Service END");
+}
+
+const client = async () => {
+    universe.logger.info("Test Everblack Client");
+    let Queue = universe.everblack.Queue;
+
+    let queue = await Queue.at(qlocation);
+
+    let response = await queue.request({ test: 'Test XY' }, 100);
+    universe.logger.info('Queue Response', response);
+
+    universe.logger.info("Test Everblack Client END");
+}
+
+
+const privservice = async () => {
+    universe.logger.info("Test Everblack Service");
+    let Queue = universe.everblack.Queue;
+
+    let queue = await Queue
+        .at(pqlocation)
+        .signon(servicepair)    // owner of servicepair is admin
+        .createIfMissing();
+
+    await queue.invite(clientpair.pub);
+
+    queue.handle((request, response) => {
+        universe.logger.info('Private Queue Request', request);
+        response.payload = request.payload;
+    })
+
+    universe.logger.info("Test Everblack Service END");
+}
+
+const privclient = async () => {
+    universe.logger.info("Test Everblack Client");
+    let Queue = universe.everblack.PrivateQueue;
+
+    let queue = Queue
+        .at(clocation)
+        .signon(clientpair);
+
+    let response = await queue.request({ test: 'Test' });
+    universe.logger.info('Private Queue Response', response);
+
+    universe.logger.info("Test Everblack Client END");
+}
 
 const alice = async () => {
     universe.logger.info("Test Everblack Alice");
@@ -22,7 +104,7 @@ const alice = async () => {
     let Channel = universe.everblack.Channel;
 
     let channel = await Channel
-        .at(location)
+        .at(clocation)
         .createIfMissing();  // current user is admin
     universe.mychannel = channel;
 
@@ -42,22 +124,26 @@ const bob = async () => {
 
     let Channel = universe.everblack.Channel;
     let channel = await Channel
-        .at(location)
+        .at(clocation)
         .join();
     channel.onMessage((msg) => console.log('bob>', msg));               // show arriving messages including all previous
 
-    await channel.send("message 2 from bobB");
+    await channel.send("message 3 from bobB");
 
     // await universe.identity.leave();
     universe.logger.info("Test Everblack Bob END");
 }
 
 (async () => {
-    /*
-        await alice();
-        await timeout(500);
-        await universe.identity.leave();
-    */
+/*
+    await alice();
+    await timeout(500);
+    await universe.identity.leave();
     await bob();
+*/
+
+    await service();
+    // await timeout(300);
+    await client();
 })();
 
