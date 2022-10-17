d4ee93eaa781b747585e48cc6ce4eee0a1e87d43
Bernhard Lukassen <bl@bernhard-lukassen.com>
2022-10-17 15:37:43 +0200

    move home to appinstance, fixes, docu

--- dsys.mjs
+++ dsys.mjs
@@ -6,6 +6,61 @@
  */
 // import { ComponentDescriptor } from '/evolux.dyncomponents';
 
-import dsys from "./www/dsys.mjs";
+export default async (universe) => {
+    const services              = universe.services;
+    const components            = services.components;
+    const ComponentDescriptor   = components.ComponentDescriptor;
 
-export default dsys;
+    /*
+     * Components
+     */
+    const pubsub =       ComponentDescriptor({
+                                                 id:             'pubsub',
+                                                 displayName:    'publish/subscribe infrastructure in the universe',
+                                                 category:       'universe',
+                                                 href:           '/evolux.pubsub',
+                                             });
+    const gun =       ComponentDescriptor({
+                                              id:             'gun',
+                                              displayName:    'distributed DB from universe',
+                                              category:       'universe',
+                                              href:           '/terra.gun',
+                                          });
+    const everblack = ComponentDescriptor({
+                                              id:             'everblack',
+                                              displayName:    'encryption & security for thoregon',
+                                              category:       'thoregon',
+                                              href:           '/evolux.everblack',
+                                          });
+    /*
+        const matter =    ComponentDescriptor({
+            id:             'matter',
+            displayName:    'matter in the universe',
+            category:       'universe',
+            href:           '/evolux.matter',
+        });
+    */
+
+    // install 'gun' to have synced distributed DB available
+    await components.install(gun);
+    await components.resolve(gun.id);
+    await components.start(gun.id);
+
+    // install 'gun' to have synced distributed DB available
+    await components.install(pubsub);
+    await components.resolve(pubsub.id);
+    await components.start(pubsub.id);
+
+    // install 'everblack' to have secure end2end encryption within the distributed DB
+    await components.install(everblack);
+    await components.resolve(everblack.id);
+    await components.start(everblack.id);
+
+    // install 'matter' to have fast queries available
+    /*
+        await components.install(matter);
+        await components.resolve(matter.id);
+        await components.start(matter.id);
+    */
+
+};;
--- dsys_x.mjs
+++ dsys_x.mjs
@@ -6,6 +6,69 @@
  */
 // import { ComponentDescriptor } from '/evolux.dyncomponents';
 
-import dsysx from "./www/dsys_x.mjs";
+export default async (universe) => {
+    const services              = universe.services;
+    const components            = services.components;
+    const ComponentDescriptor   = components.ComponentDescriptor;
+    /*
+     * Components
+     */
+    const www =       ComponentDescriptor({
+                                                 id:             'www',
+                                                 displayName:    'connect to the web world',
+                                                 category:       'universe',
+                                                 href:           '/evolux.web',
+                                             });
+    // install 'web'. provides universe wide persistence
+    await components.install(www);
+    await components.resolve(www.id);
+    await components.start(www.id);
 
-export default dsysx;
+    /*
+     * Thoregon Components
+     */
+    const archetim =  ComponentDescriptor({
+                                              id:             'archetim',
+                                              displayName:    'universe wide graph DB',
+                                              category:       'universe',
+                                              href:           '/thoregon.archetim',
+                                          });
+    const identity  = ComponentDescriptor({
+                                              id:             'identity',
+                                              displayName:    'thoregon distributed identites',
+                                              category:       'thoregon',
+                                              href:           '/thoregon.identity',
+                                          });
+
+    const truCloud =  ComponentDescriptor({
+                                              id:             'truCloud',
+                                              displayName:    'truCloud supporting truServerless',
+                                              category:       'universe',
+                                              href:           '/thoregon.truCloud',
+                                          });
+
+
+    // install 'archetim'. provides universe wide persistence
+    await components.install(archetim);
+    await components.resolve(archetim.id);
+    await components.start(archetim.id);
+    // install 'heavymatter'. it is essential to store files and other big data
+    // await components.install(heavymatter);
+    // await components.resolve(heavymatter.id);
+    // await components.start(heavymatter.id);
+
+    // install 'tru4D'. it is essential to have transactions and the eventstore available
+    // await components.install(tru4D);
+    // await components.resolve(tru4D.id);
+    // await components.start(tru4D.id);
+
+    // install 'truCloud'. this is the magic
+    await components.install(truCloud);
+    await components.resolve(truCloud.id);
+    await components.start(truCloud.id);
+
+    // install 'identity'. it is essential to have identities for DDDD available
+    await components.install(identity);
+    await components.resolve(identity.id);
+    await components.start(identity.id);
+};;
