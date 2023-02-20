d25299fde2063a03ede60e4e0740c2ed7229c5ba
Bernhard Lukassen <bl@bernhard-lukassen.com>
2023-02-20 16:35:30 +0100

    introduce thoregon.neuland for p2p

---
+++ thoregonsystem.mjs
@@ -0,0 +1,108 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ * @licence: MIT
+ * @see: {@link https://github.com/Thoregon}
+ */
+
+import SEA                   from '/evolux.everblack/lib/crypto/sea.mjs'
+import GunService            from '/terra.gun/lib/gunservice.mjs';
+import NodeLifecycleEmitter  from "/thoregon.neuland/modules/nodepeer/nodelifecycleemitter.mjs";
+import { Automerge, Peer }   from "/thoregon.neuland/modules/nodepeer/index.mjs";
+import NeulandStorageAdapter from "/thoregon.neuland/modules/nodepeer/fsneulandstorageadapter.mjs";
+import NeulandDB             from "/thoregon.neuland/src/storage/neulanddb.mjs";
+import P2PNetworkPolicy      from "/thoregon.neuland/src/p2p/p2pnetworkpolicy.mjs";
+import PeerJSNetworkAdapter  from "/thoregon.neuland/src/p2p/peerjsnetworkadapter.mjs";
+import SyncManager           from "/thoregon.neuland/src/sync/syncmanager.mjs";
+import MQ                    from "/thoregon.neuland/src/mq/mq.mjs";
+import IdentityReflection    from '/thoregon.identity/lib/identityreflection.mjs';
+import Dorifer               from '/thoregon.truCloud/lib/dorifer.mjs';
+import WebserviceController  from '/evolux.web//lib/webservicecontroller.mjs';
+
+//
+// crypto, safety & security
+//
+
+universe.$everblack = SEA;
+universe.$lifecycle = new NodeLifecycleEmitter();
+
+//
+// network policies & adapters,
+//
+
+universe.$Peer      = Peer;
+universe.$netconfig = {
+    peerid: 'ynGhbGJjEh3BCNH1mSBTykj89a7PXNzO',
+        policies: [P2PNetworkPolicy],
+        p2p: { adapters: [PeerJSNetworkAdapter] }
+};
+
+
+const netopt = {};
+universe.$net = universe.netconfig.policies.map((Policy) =>  new Policy(netopt));
+
+//
+// crdt
+//
+
+universe.$Automerge = Automerge;
+universe.$syncmgr   = SyncManager.setup();
+universe.$mq        = MQ.setup();
+
+//
+// components
+//
+
+const neuland    = new NeulandDB();
+const gunservice = new GunService();
+const identity   = new IdentityReflection();
+const dorifer    = new Dorifer();
+const wsc        = new WebserviceController();
+
+neuland.init(NeulandStorageAdapter, universe.NEULAND_STORAGE_OPT);
+neuland.start();
+await gunservice.start();
+await identity.start();
+await dorifer.start();
+await wsc.start();
+if (universe.DEV?.ssi) {
+    const SSI = universe.DEV?.ssi;
+    const spec = (await import("./agent_0.config.mjs")).default;
+    await universe.Identity.useIdentity(SSI);
+    await universe.Agent.addServiceSpec(spec);
+}
+
+//
+// information functions
+//
+
+universe.$p2ppolicy = () => universe.net[0];
+universe.$p2padapter = () => universe.p2ppolicy().net[0];
+
+//
+// awake agent when SSI is avialable
+//
+await agent.prepare();
+
+// don't need a double lifecycle handling
+// universe.lifecycle.addEventListener('prepare', () => {
+//     neuland.init(NeulandStorageAdapter, universe.NEULAND_STORAGE_OPT);
+// });
+//
+// universe.lifecycle.addEventListener('start', async () => {
+//     neuland.start();
+//     await gunservice.start();
+//     await identity.start();
+//     await dorifer.start();
+//     await wsc.start();
+//     if (universe.DEV?.ssi) {
+//         const SSI = universe.DEV?.ssi;
+//         const spec = (await import("./agent_0.config.mjs")).default;
+//         await universe.Identity.useIdentity(SSI);
+//         await universe.Agent.addServiceSpec(spec);
+//     }
+//
+// // awake agent when SSI is avialable
+//     await agent.prepare();
+// });
--- universe.config.mjs
+++ universe.config.mjs
@@ -7,20 +7,6 @@
 import os                                   from '/os';
 import { version }                          from '/process';
 
-import Controller, { ComponentsWatcher }    from '/evolux.dyncomponents';
-
-import { tservices }                        from '/evolux.universe';
-
-import SEA                                  from '/evolux.everblack/lib/crypto/sea.mjs'
-import GunService                           from '/terra.gun/lib/gunservice.mjs';
-import IdentityReflection                   from '/thoregon.identity/lib/identityreflection.mjs';
-import Dorifer                              from '/thoregon.truCloud/lib/dorifer.mjs';
-import WebserviceController                 from '/evolux.web//lib/webservicecontroller.mjs';
-
-// import dsys                                 from "./dsys.mjs";
-// import dsysp                                from "./dsys.sovereign.mjs";
-// import dsysx                                from "./dsys_x.mjs";
-
 //
 // JS engine independence
 //
@@ -28,14 +14,9 @@ import npath from "/path";
 export const path = npath;
 
 // gun
-export const gunpeers = ['http://185.11.139.203:8765/gun', 'http://127.0.0.1:8765/gun'];
-//export const gunpeers           = ['http://185.11.139.203:8765/gun'/*, 'https://matter.thoregon.io:8765/gun'*/];
+export const gunpeers           = ['http://185.11.139.203:8765/gun'/*, 'https://matter.thoregon.io:8765/gun'*/];
 
-// todo: move to seperate config e.g. 'tru4d.config.mjs'
-export const responsibilities   = [
-    'thoregon.service',
-    'identity.services.provider'
-];
+export const NEULAND_STORAGE_OPT = { location: 'data', name: 'neuland' };    // can override: writeCount, writeInterval
 
 //
 // define agent
@@ -85,45 +66,13 @@ export const GET_SECRET_WORKER = async () => {
 // initialize unviverse wide services an functions
 //
 
-export const everblack = SEA;
+universe.atDawn(async (universe) => {
+    await import('./thoregonsystem.mjs');
 
-universe.atDawn(async universe => {
-    // const componentLocation     = 'components';
-    // const componentController   = Controller.baseCwd('ThoregonComponentController');
-    // tservices().components = componentController;
-    //
-    // // now setup the basic distributed system
-    // await dsysp(universe);      // first the sovereign settings
-    // await dsys(universe);       // basic system
-    // await dsysx(universe);      // extended system
-    //
-    // // now install all other components
-    // componentController.addPlugin(ComponentsWatcher.watch(componentLocation));
-    // // todo: Refactor LocationWatcher to use 'matter.components'
-
-    const gunservice = new GunService();
-    await gunservice.start();
-
-    const identity = new IdentityReflection();
-    await identity.start();
-
-    const dorifer = new Dorifer();
-    await dorifer.start();
-
-    const wsc = new WebserviceController();
-    await wsc.start();
-
-    if (universe.DEV?.ssi) {
-        const SSI = universe.DEV?.ssi;
-        const spec = (await import("./agent_0.config.mjs")).default;
-        await universe.Identity.useIdentity(SSI);
-        await universe.Agent.addServiceSpec(spec);
-    }
-
-    // awake agent when SSI is avialable
-    await agent.prepare();
+    universe.lifecycle.triggerPrepare();
+    universe.lifecycle.triggerStart();
 });
 
-universe.atDusk(async universe => {
-    // await tservices().components.exit();
+universe.atDusk(async (universe, code) => {
+    universe.lifecycle?.triggerExit(code);
 });
